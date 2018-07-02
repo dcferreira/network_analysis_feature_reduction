dc2c4747b587c60448827bd824780a48af71bb81
Daniel Ferreira <dcferreira@users.noreply.github.com>
2018-07-02 18:37:12 +0200

    added more parameters to run.py

--- run.py
+++ run.py
@@ -56,44 +56,60 @@ def tsne(args):
     aggregated(mod, data)
 
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 subparsers = parser.add_subparsers(dest='method')
 subparsers.required = True
 parser.add_argument('datapath', type=str)
-parser.add_argument('--size', type=int, default=2)
-parser.add_argument('--number', type=int, default=5)  # number of models used to average
+parser.add_argument('--size', type=int, default=2, help='Reduce data to this dimension')
+parser.add_argument('--number', type=int, default=5, help='Number of models used to average')
 
 
 # pca
-parser_pca = subparsers.add_parser('pca')
+parser_pca = subparsers.add_parser('pca', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser_pca.set_defaults(func=pca)
 
 
+# SemiSupNN arguments_parent
+parent_parser = argparse.ArgumentParser(add_help=False, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parent_parser.add_argument("--reconstruct_loss", type=str, help='One of "binary-crossentropy", "mse".',
+                           default='binary-crossentropy')
+parent_parser.add_argument('--reconstruct_weight', type=float, default=0.1,
+                           help='Weight of reconstruction loss. Label loss is always 1.0')
+parent_parser.add_argument('--enc_regularizer_weight', type=float, default=0.1,
+                           help='Weight of l2 regularization in the encoder')
+parent_parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
+parent_parser.add_argument('--lr_decay', type=float, default=1e-5, help='Decay of learning rate')
+parent_parser.add_argument('--dec_regularizer_weight', type=float, default=0.,
+                           help='Weight of the l2 regularization in the decoder')
+
+
 # unsup_ae
-parser_unsup_ae = subparsers.add_parser('unsup_ae')
-parser_unsup_ae.add_argument('--loss', type=str, help='One of "binary-crossentropy", "mse".',
-                             default='binary-crossentropy')
+parser_unsup_ae = subparsers.add_parser('unsup_ae', parents=[parent_parser],
+                                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser_unsup_ae.set_defaults(func=unsup_ae)
 
 
 # sup_cats_ae
-parser_sup_cats_ae = subparsers.add_parser('sup_cats_ae')
+parser_sup_cats_ae = subparsers.add_parser('sup_cats_ae', parents=[parent_parser],
+                                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser_sup_cats_ae.set_defaults(func=sup_cats_ae)
 
 
 # bin_ae
-parser_bin_ae = subparsers.add_parser('bin_ae')
+parser_bin_ae = subparsers.add_parser('bin_ae', parents=[parent_parser],
+                                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser_bin_ae.set_defaults(func=bin_ae)
 
 
 # cats_ae
-parser_cats_ae = subparsers.add_parser('cats_ae')
+parser_cats_ae = subparsers.add_parser('cats_ae', parents=[parent_parser],
+                                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser_cats_ae.set_defaults(func=cats_ae)
 
 
 # tsne
-parser_tsne = subparsers.add_parser('tsne')
+parser_tsne = subparsers.add_parser('tsne', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser_tsne.set_defaults(func=tsne)
 
 
