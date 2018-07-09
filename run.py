01dcf03047b92ec8c6fa3f23cbf02d4bdad8b4a1
Daniel Ferreira <dcferreira@users.noreply.github.com>
2018-07-09 14:01:25 +0200

    added many input parameters for script

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-import numpy as np
+import json
 from data import Data
 from models import *
 from classifiers import test_model, Aggregator
@@ -19,40 +19,83 @@ def pca(args):
     test_model(data, mod)
 
 
-def aggregated(mod, data):
-    mod.train(verbose=0)
+def _aggregated(mod, data, verbose):
+    if verbose:
+        mod.train(verbose=2, **args.train)  # force verbose=2 for keras models
+    else:
+        mod.train(**args.train)
     mod.get_metrics(data)
-    mod.apply_op(np.mean, display_scores=True)
-    mod.apply_op(np.std, display_scores=True)
+    mod.mean(display_scores=True)
+    mod.std(display_scores=True)
+
+
+def aggregated(mod, data):
+    _aggregated(mod, data, False)
+
+
+def aggregated_keras(mod, data):
+    _aggregated(mod, data, True)
 
 
 def unsup_ae(args):
     data = get_data(args)
-    mod = Aggregator(UnsupNN, args.number, data, args.size, reconstruct_loss=args.loss)
-    aggregated(mod, data)
+    mod = Aggregator(UnsupNN, args.number, data, args.size,
+                     reconstruct_loss=args.reconstruct_loss, reconstruct_weight=args.reconstruct_weight,
+                     enc_regularizer_weight=args.enc_regularizer_weight,
+                     dec_regularizer_weight=args.dec_regularizer_weight,
+                     lr=args.lr, lr_decay=args.lr_decay)
+    aggregated_keras(mod, data)
 
 
 def sup_cats_ae(args):
     data = get_data(args)
-    mod = Aggregator(SupNN, args.number, data, args.size)
-    aggregated(mod, data)
+    mod = Aggregator(SupNN, args.number, data, args.size,
+                     reconstruct_loss=args.reconstruct_loss, reconstruct_weight=args.reconstruct_weight,
+                     enc_regularizer_weight=args.enc_regularizer_weight,
+                     dec_regularizer_weight=args.dec_regularizer_weight,
+                     lr=args.lr, lr_decay=args.lr_decay)
+    aggregated_keras(mod, data)
+    mod.get_own_metrics(data, categories=True)
+    mod.mean(display_scores=True)
+    mod.std(display_scores=True)
 
 
 def bin_ae(args):
     data = get_data(args)
-    mod = Aggregator(SemisupNN, args.number, data, args.size, categories=False)
-    aggregated(mod, data)
+    mod = Aggregator(SemisupNN, args.number, data, args.size, categories=False,
+                     reconstruct_loss=args.reconstruct_loss, reconstruct_weight=args.reconstruct_weight,
+                     enc_regularizer_weight=args.enc_regularizer_weight,
+                     dec_regularizer_weight=args.dec_regularizer_weight,
+                     lr=args.lr, lr_decay=args.lr_decay)
+    aggregated_keras(mod, data)
+    mod.get_own_metrics(data, categories=False)
+    mod.mean(display_scores=True)
+    mod.std(display_scores=True)
 
 
 def cats_ae(args):
     data = get_data(args)
-    mod = Aggregator(SemisupNN, args.number, data, args.size, categories=True)
-    aggregated(mod, data)
+    mod = Aggregator(SemisupNN, args.number, data, args.size, categories=True,
+                     reconstruct_loss=args.reconstruct_loss, reconstruct_weight=args.reconstruct_weight,
+                     enc_regularizer_weight=args.enc_regularizer_weight,
+                     dec_regularizer_weight=args.dec_regularizer_weight,
+                     lr=args.lr, lr_decay=args.lr_decay)
+    aggregated_keras(mod, data)
+    mod.get_own_metrics(data, categories=True)
+    mod.mean(display_scores=True)
+    mod.std(display_scores=True)
 
 
 def tsne(args):
     data = get_data(args)
-    mod = Aggregator(TSNE, args.number, data, args.size)
+    mod = Aggregator(TSNE, args.number, data, args.size,
+                     perplexity=args.perplexity, early_exaggeration=args.early_exaggeration,
+                     learning_rate=args.learning_rate, n_iter=args.n_iter,
+                     n_iter_without_progress=args.n_iter_without_progress,
+                     min_grad_norm=args.min_grad_norm, metric=args.metric,
+                     init=args.init, verbose=args.verbose, random_state=args.random_state,
+                     method=args.method, angle=args.angle
+                     )
     aggregated(mod, data)
 
 
@@ -63,6 +106,8 @@ subparsers.required = True
 parser.add_argument('datapath', type=str)
 parser.add_argument('--size', type=int, default=2, help='Reduce data to this dimension')
 parser.add_argument('--number', type=int, default=5, help='Number of models used to average')
+parser.add_argument('--train', type=json.loads, default={},
+                    help='Dictionary (in the form of string) that is passed to the train command as arguments')
 
 
 # pca
@@ -110,6 +155,18 @@ parser_cats_ae.set_defaults(func=cats_ae)
 
 # tsne
 parser_tsne = subparsers.add_parser('tsne', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser_tsne.add_argument('--perplexity', type=float, default=30.0)
+parser_tsne.add_argument('--early_exaggeration', type=float, default=30.0)
+parser_tsne.add_argument('--learning_rate', type=float, default=200.0)
+parser_tsne.add_argument('--n_iter', type=int, default=1000)
+parser_tsne.add_argument('--n_iter_without_progress', type=int, default=300)
+parser_tsne.add_argument('--min_grad_norm', type=float, default=1e-07)
+parser_tsne.add_argument('--metric', type=str, default='euclidean')
+parser_tsne.add_argument('--init', type=str, default='random')
+parser_tsne.add_argument('--verbose', type=int, default=0)
+parser_tsne.add_argument('--random_state', type=int, default=None)
+parser_tsne.add_argument('--method', type=str, default='barnes_hut')
+parser_tsne.add_argument('--angle', type=float, default=0.5)
 parser_tsne.set_defaults(func=tsne)
 
 
