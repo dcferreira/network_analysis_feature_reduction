a814174b0ac9163abf5e29ae11f00697348ffef4
Daniel Ferreira <dcferreira@users.noreply.github.com>
2019-02-19 15:09:50 +0100

    don't need to pass first argument to python (it's already consumed in the entrypoint script)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 if [ "$1" = "tfm" ]; then
     cd bokeh_stream && PYTHONPATH=.. bokeh serve . --address 0.0.0.0 --allow-websocket-origin '*'
 elif [ "$1" = "unsw15" ]; then
-    python -u run_unsw15.py $@
+    python -u run_unsw15.py ${@:2}
 elif [ "$1" = "csv" ]; then
-    python -u run_csv.py $@
+    python -u run_csv.py ${@:2}
 fi
